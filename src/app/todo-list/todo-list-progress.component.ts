8be4ae9517e67a65ceab02c9948acc9c02017d9b
eyalvardi <evardi@gmail.com>
2018-03-12 15:19:33 +0200

    todo list demo for magic software v2 UI

---
+++ src/app/todo-list/todo-list-progress.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {TodoListService} from "./todo-list.service";
+
+@Component({
+    selector: 'todo-list-progress',
+    template: `
+        <div class="d-flex justify-content-end">
+            Status: &nbsp;
+            <div class="d-inline-block progress ">
+                <div class="bg-info border" [ngStyle]="{ width: blService.donePercentage * 100 + '%' }">
+                    &nbsp;
+                </div>
+            </div>
+            <div class="d-inline-block" style="padding: 0px 3px">
+                {{ blService.donePercentage | percent}}
+            </div>
+        </div>
+
+    `,
+    styles: []
+})
+export class TodoListProgressComponent {
+
+    constructor(public blService:TodoListService) {
+    }
+
+}
